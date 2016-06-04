88c31b8c54929f468cd1f10cac18c49f97a96ac4
HarryR <harry@midnight-labs.org>
2016-06-04 11:50:12 +0100

    Submit all files from a directory

---
+++ src/utils/submit_dir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+	echo "Usage: `basename $0` <dir> [dir ...]"
+	exit 1
+fi
+
+for DIR in "${@:1}"
+do
+	if [[ ! -d "$DIR" ]]; then
+		echo "Ignoring $DIR - not a directory"
+		continue
+	fi
+	for FILE in `ls -1 $DIR`
+	do
+		FULLPATH=$DIR/$FILE
+		if [[ -f "$FULLPATH" ]]; then
+			curl http://localhost:9003/api/task -F file="@$FULLPATH" -F timeout=120
+		fi
+	done
+done 
