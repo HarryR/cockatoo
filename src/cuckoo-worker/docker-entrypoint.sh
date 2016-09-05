0658323ef7832696d28af13da7b39e7559cb462d
HarryR <HarryR@users.noreply.github.com>
2016-09-05 16:47:41 +0100

    Fixed worker entrypoint for updated vmcloak

--- src/cuckoo-worker/docker-entrypoint.sh
+++ src/cuckoo-worker/docker-entrypoint.sh
@@ -30,8 +30,8 @@ if [[ "$CUCKOO_MACHINERY" = "virtualbox" ]]; then
 			# First purge, then register it again
 			echo "Importing VM: $vmname - IP: $vmip"
 			#/cuckoo/utils/machine.py --delete $vmname || true
-			vmcloak-snapshot $vmname vm-$vmname $vmip
-			vmcloak-register vm-$vmname /cuckoo
+			vmcloak snapshot $vmname vm-$vmname $vmip
+			vmcloak register vm-$vmname /cuckoo
 			VM_N=$((VM_N + 1))
 		done
 	else
